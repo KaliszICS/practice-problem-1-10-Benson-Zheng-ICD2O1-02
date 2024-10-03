f9501cc37c142d1b854312a889f636ca472b3983
Benson-Zheng-ICD2O1-02 <benson.zheng3@student.tdsb.on.ca>
2024-10-03 14:31:56 +0000

    9999

--- PP1_10.py
+++ PP1_10.py
@@ -1,31 +1,51 @@
-
+import math
 
 def q1(): 
   #Write Assignment code here
 
+  user_input = input("Input a number: ")
+  user_input = float(user_input)
+  print (math.sqrt(user_input))
+
 
 def q2(): 
   #Write Assignment code here
+  user_input = input("Input a number: ")
+  user_input = int(user_input)
+  print (math.isqrt(user_input))
 
 
 def q3(): 
   #Write Assignment code here
+  user_input = input("Input a number: ")
+  user_input = float(user_input)
+  print (math.floor(user_input))
 
 
 def q4(): 
   #Write Assignment code here
+  user_input = input("Input a number: ")
+  user_input = float(user_input)
+  print (math.ceil(user_input))
 
 
 def q5(): 
   #Write Assignment code here
+  user_input = input("Input a number: ")
+  user_input2 = input("Input another number: ")
+  user_input = float(user_input)
+  user_input2 = float(user_input2)
+  product = (user_input*user_input2)/2
+  print(math.trunc(product))
+
 
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+# q1()
+# q2()
+# q3()
+# q4()
+# q5()
